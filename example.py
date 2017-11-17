bf81aeb47a8d18a7bcc5b60478643bbc1332469e
wenxizhu <wenxi.zhu@intel.com>
2017-11-17 14:57:49 +0800

    Convert indentation from tab to spaces

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ iter = 3
 #
 # Create model
 #
-model = alexnet.alexnet()
+    model = alexnet.alexnet()
 
 #
 # Use case 1: use it as context-manager
@@ -20,22 +20,22 @@ model = alexnet.alexnet()
 
 # profiler will measure the following 3 iterations.
 with Profiling(model) as p:
-	for i in xrange(iter):
-		# Forward:
-		output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
+    for i in xrange(iter):
+        # Forward:
+        output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
 
-		# Backward:
-		grads = torch.ones(2, 1000)
-		output.backward(grads);
+        # Backward:
+        grads = torch.ones(2, 1000)
+        output.backward(grads);
 
 # profiler won't measure the following 2 extra iterations, since they are out of profiler's range.
 for i in xrange(2):
-	# Forward:
-	output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
+    # Forward:
+    output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
 
-	# Backward:
-	grads = torch.ones(2, 1000)
-	output.backward(grads);
+    # Backward:
+    grads = torch.ones(2, 1000)
+    output.backward(grads);
 
 # profiler will print the 3 iterations result rather than 5.
 print(p)
@@ -49,22 +49,22 @@ print(p)
 # p = Profiling(model).start()
 
 # for i in xrange(iter):
-# 	# Forward:
-# 	output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
+#   # Forward:
+#   output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
 
-# 	# Backward:
-# 	grads = torch.ones(2, 1000)
-# 	output.backward(grads);
+#   # Backward:
+#   grads = torch.ones(2, 1000)
+#   output.backward(grads);
 
 # p.stop()
 
 # # profiler won't measure the following 2 extra iterations, since they are out of profiler's range.
 # for i in xrange(2):
-# 	# Forward:
-# 	output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
+#   # Forward:
+#   output = model.forward(Variable(torch.ones(2, 3, 224, 224), requires_grad=True))
 
-# 	# Backward:
-# 	grads = torch.ones(2, 1000)
-# 	output.backward(grads);
+#   # Backward:
+#   grads = torch.ones(2, 1000)
+#   output.backward(grads);
 
 # print(p)
--- profiling.py
+++ profiling.py
@@ -5,109 +5,109 @@ from torch.autograd import Function
 import time
 
 class Profiling(object):
-	def __init__(self, model):
-		if isinstance(model, torch.nn.Module) is False:
-			print("Not a valid model, please provide a 'nn.Module' instance.")
+    def __init__(self, model):
+        if isinstance(model, torch.nn.Module) is False:
+            print("Not a valid model, please provide a 'nn.Module' instance.")
 
-		self.model = model
-		self.record = {'forward':[], 'backward': []}
-		self.profiling_on = True
-		self.origin_call = {}
-		self.hook_done = False
-		self.layer_num = 0
+        self.model = model
+        self.record = {'forward':[], 'backward': []}
+        self.profiling_on = True
+        self.origin_call = {}
+        self.hook_done = False
+        self.layer_num = 0
 
-	def __enter__(self):
-		self.start()
+    def __enter__(self):
+        self.start()
 
-		return self
+        return self
 
-	def __exit__(self, *args):
-		self.stop()
+    def __exit__(self, *args):
+        self.stop()
 
-	def __str__(self):
-		ret = ""
+    def __str__(self):
+        ret = ""
 
-		iter = len(self.record['forward']) / self.layer_num
+        iter = len(self.record['forward']) / self.layer_num
 
-		for i in xrange(iter):
-			ret += "\n================================= Iteration {} =================================\n".format(i + 1)
-	
-			ret += "\nFORWARD TIME:\n\n"
-			for j in xrange(self.layer_num):
-				record_item = self.record['forward'][i * self.layer_num + j]
-				ret += "layer{:3d}:          {:.6f} ms 			({})\n".format(j + 1, record_item[2] - record_item[1], record_item[0])
+        for i in xrange(iter):
+            ret += "\n================================= Iteration {} =================================\n".format(i + 1)
+    
+            ret += "\nFORWARD TIME:\n\n"
+            for j in xrange(self.layer_num):
+                record_item = self.record['forward'][i * self.layer_num + j]
+                ret += "layer{:3d}:          {:.6f} ms          ({})\n".format(j + 1, record_item[2] - record_item[1], record_item[0])
 
-			ret += "\nBACKWARD TIME:\n\n"
-			for j in (xrange(self.layer_num)):
-				record_item = self.record['backward'][i * self.layer_num + self.layer_num - j - 1]
-				ret += "layer{:3d}:          {:.6f} ms 			({})\n".format(j + 1, record_item[2] - record_item[1], record_item[0])
+            ret += "\nBACKWARD TIME:\n\n"
+            for j in (xrange(self.layer_num)):
+                record_item = self.record['backward'][i * self.layer_num + self.layer_num - j - 1]
+                ret += "layer{:3d}:          {:.6f} ms          ({})\n".format(j + 1, record_item[2] - record_item[1], record_item[0])
 
-		return ret
+        return ret
 
-	def start(self):
-		if self.hook_done is False:
-			self.hook_done = True
-			self.hook_modules(self.model)
+    def start(self):
+        if self.hook_done is False:
+            self.hook_done = True
+            self.hook_modules(self.model)
 
-		self.profiling_on = True
+        self.profiling_on = True
 
-		return self
+        return self
 
-	def stop(self):
-		self.profiling_on = False
+    def stop(self):
+        self.profiling_on = False
 
-		return self
+        return self
 
-	def hook_modules(self, module):
+    def hook_modules(self, module):
 
-		this_profiler = self
+        this_profiler = self
 
-		sub_modules = module.__dict__['_modules']
+        sub_modules = module.__dict__['_modules']
 
-		for name, sub_module in sub_modules.items():
-			# nn.Module is the only thing we care about.
-			if sub_module is None or isinstance(sub_module, torch.nn.Module) is False:
-				break
+        for name, sub_module in sub_modules.items():
+            # nn.Module is the only thing we care about.
+            if sub_module is None or isinstance(sub_module, torch.nn.Module) is False:
+                break
 
-			if isinstance(sub_module, torch.nn.Container) or isinstance(sub_module, torch.nn.Sequential):
-				#
-				# nn.Container or nn.Sequential who have sub nn.Module. Recursively visit and hook their decendants.
-				#
-				self.hook_modules(sub_module)
-			else:
+            if isinstance(sub_module, torch.nn.Container) or isinstance(sub_module, torch.nn.Sequential):
+                #
+                # nn.Container or nn.Sequential who have sub nn.Module. Recursively visit and hook their decendants.
+                #
+                self.hook_modules(sub_module)
+            else:
 
-				self.layer_num += 1
-				#
-				# nn.Module who doesn't have sub nn.Module, hook it.
-				#
+                self.layer_num += 1
+                #
+                # nn.Module who doesn't have sub nn.Module, hook it.
+                #
 
-				# Wrapper function to "__call__", with time counter in it.
-				def wrapper_call(self, *input, **kwargs):
-					start_time = time.time()
-					result = this_profiler.origin_call[self.__class__](self, *input, **kwargs)
-					stop_time = time.time()
+                # Wrapper function to "__call__", with time counter in it.
+                def wrapper_call(self, *input, **kwargs):
+                    start_time = time.time()
+                    result = this_profiler.origin_call[self.__class__](self, *input, **kwargs)
+                    stop_time = time.time()
 
-					that = self
-					def backward_pre_hook(*args):
-						if (this_profiler.profiling_on):
-							this_profiler.record['backward'].append((that, time.time()))
+                    that = self
+                    def backward_pre_hook(*args):
+                        if (this_profiler.profiling_on):
+                            this_profiler.record['backward'].append((that, time.time()))
 
-					result.grad_fn.register_pre_hook(backward_pre_hook);
+                    result.grad_fn.register_pre_hook(backward_pre_hook);
 
-					if (this_profiler.profiling_on):
-						global record
-						this_profiler.record['forward'].append((self, start_time, stop_time))
+                    if (this_profiler.profiling_on):
+                        global record
+                        this_profiler.record['forward'].append((self, start_time, stop_time))
 
 
-					return result
+                    return result
 
-				# Replace "__call__" with "wrapper_call".
-				if sub_module.__class__ not in this_profiler.origin_call:
-					this_profiler.origin_call.update({sub_module.__class__: sub_module.__class__.__call__})
-					sub_module.__class__.__call__ = wrapper_call
+                # Replace "__call__" with "wrapper_call".
+                if sub_module.__class__ not in this_profiler.origin_call:
+                    this_profiler.origin_call.update({sub_module.__class__: sub_module.__class__.__call__})
+                    sub_module.__class__.__call__ = wrapper_call
 
-				def backward_post_hook(*args):
-					if (this_profiler.profiling_on):
-						this_profiler.record['backward'][-1] = (this_profiler.record['backward'][-1][0], this_profiler.record['backward'][-1][1], time.time()) 
-			
-				sub_module.register_backward_hook(backward_post_hook)
+                def backward_post_hook(*args):
+                    if (this_profiler.profiling_on):
+                        this_profiler.record['backward'][-1] = (this_profiler.record['backward'][-1][0], this_profiler.record['backward'][-1][1], time.time()) 
+            
+                sub_module.register_backward_hook(backward_post_hook)
