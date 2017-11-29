dc3daa8237d10a010de3d0a426c6f201582da288
wenxizhu <wenxi.zhu@intel.com>
2017-11-29 12:37:27 +0800

    Correct the mistake of calculating backward time.

--- profiling.py
+++ profiling.py
@@ -16,6 +16,10 @@ class Profiling(object):
         self.hook_done = False
         self.layer_num = 0
 
+        self.backward_record = []
+        self.seen_backward_func = []
+        self.prev_backward_func = []
+
     def __enter__(self):
         self.start()
 
@@ -25,7 +29,13 @@ class Profiling(object):
         self.stop()
 
     def __str__(self):
+
         ret = ""
+
+        time_dict = {'forward': [], 'backward': []}
+
+        global TD
+        TD = time_dict
 
         iter = len(self.record['forward']) / self.layer_num
 
@@ -37,14 +47,38 @@ class Profiling(object):
                 record_item = self.record['forward'][i * self.layer_num + j]
                 ret += "layer{:3d}:          {:.6f} ms          ({})\n".format(j + 1, record_item[2] - record_item[1], record_item[0])
 
+                if j >= len(time_dict['forward']):
+                    assert j is len(time_dict['forward'])
+                    time_dict['forward'].append((record_item[0], record_item[2] - record_item[1]))
+                else:
+                    time_dict['forward'][j] = (time_dict['forward'][j][0], time_dict['forward'][j][1] + record_item[2] - record_item[1])
+
             ret += "\nBACKWARD TIME:\n\n"
             for j in (xrange(self.layer_num)):
                 record_item = self.record['backward'][i * self.layer_num + self.layer_num - j - 1]
+
                 try:
                     ret += "layer{:3d}:          {:.6f} ms          ({})\n".format(j + 1, record_item[2] - record_item[1], record_item[0])
+
+                    if j >= len(time_dict['backward']):
+                        assert j is len(time_dict['backward'])
+                        time_dict['backward'].append((record_item[0], record_item[2] - record_item[1]))
+                    else:
+                        time_dict['backward'][j] = (time_dict['backward'][j][0], time_dict['backward'][j][1] + record_item[2] - record_item[1])
                 except:
-                    # Oops, this layer doesn't execute backward post-hooks
+                    print("Oops, this layer doesn't execute backward post-hooks")
                     pass
+
+        ret += "\n================================= Average =================================\n"
+
+
+        ret += "\nFORWARD TIME:\n\n"
+        for i in xrange(self.layer_num):
+            ret += "layer{:3d}:          {:.6f} ms          ({})\n".format(i, time_dict['forward'][i][1] / iter, time_dict['forward'][i][0]) 
+            
+        ret += "\nBACKWARD TIME:\n\n"
+        for i in xrange(self.layer_num):
+            ret += "layer{:3d}:          {:.6f} ms          ({})\n".format(i, time_dict['backward'][i][1] / iter, time_dict['backward'][i][0]) 
 
         return ret
 
@@ -104,14 +138,35 @@ class Profiling(object):
                             that = self
                             def backward_pre_hook(*args):
                                 if (this_profiler.profiling_on):
-                                    this_profiler.record['backward'].append((that, time.time()))
-    
-                            result.grad_fn.register_pre_hook(backward_pre_hook);
-    
+                                    if that in [x[0] for x in this_profiler.record['backward']] and [x[0] for x in this_profiler.record['backward'][::-1]].index(that) < this_profiler.layer_num - 1:
+                                        pass
+                                    else:
+                                        # print("append: {}({})".format(that, hex(id(that))))
+                                        this_profiler.record['backward'].append((that, time.time()))
+                            
+                            def backward_post_hook(*args):
+                                if (this_profiler.profiling_on):
+                                    if that in [x[0] for x in this_profiler.record['backward']]:
+                                        index_of_that = len(this_profiler.record['backward']) - [x[0] for x in this_profiler.record['backward']][::-1].index(that) - 1
+                                        record_of_that = this_profiler.record['backward'][index_of_that]
+                                        assert record_of_that[0] is that
+
+                                        this_profiler.record['backward'][index_of_that] = (record_of_that[0], record_of_that[1], time.time())
+                                    else:
+                                        raise Error("Oops, this record is broken!")
+
+
+                            # result.grad_fn.register_pre_hook(backward_pre_hook);
+                            # print("=========================== for {} ===========================".format(that))
+                            this_profiler.register_backward_hook(result.grad_fn, backward_pre_hook, backward_post_hook)
+                            this_profiler.prev_backward_func.append(result.grad_fn)
+                            # print("=========================== done ===========================")
+
                             if (this_profiler.profiling_on):
-                                global record
                                 this_profiler.record['forward'].append((self, start_time, stop_time))
                     except:
+                        import sys
+                        print("Oops:", sys.exc_info())
                         pass
 
                     return result
@@ -123,8 +178,28 @@ class Profiling(object):
 
                 sub_module.__hooked = True
 
-                def backward_post_hook(*args):
-                    if (this_profiler.profiling_on):
-                        this_profiler.record['backward'][-1] = (this_profiler.record['backward'][-1][0], this_profiler.record['backward'][-1][1], time.time()) 
+                 
             
-                sub_module.register_backward_hook(backward_post_hook)
+                # sub_module.register_backward_hook(backward_post_hook)
+
+    def register_backward_hook(self, grad_fn, backward_pre_hook, backward_post_hook):
+        if grad_fn in self.prev_backward_func:
+            return
+
+
+        # print("register pre-hook for {}".format(grad_fn))
+        grad_fn.register_pre_hook(backward_pre_hook);
+        grad_fn.register_hook(backward_post_hook);
+        # grad_fn.register_pre_hook(backward_pre_hook)
+        if grad_fn not in self.seen_backward_func:
+            self.seen_backward_func.append(grad_fn)
+
+        next_functions = grad_fn.next_functions
+        if next_functions is not () and len(next_functions) is not 0:
+            for k, v in next_functions:
+                if k is not None:
+                    if k not in self.seen_backward_func:
+                        self.seen_backward_func.append(k)
+                        self.register_backward_hook(k, backward_pre_hook, backward_post_hook)
+
+TD = None
